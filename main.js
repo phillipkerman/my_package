aad1213128f6196c568ffa5a625f42d39bb5b264
phillipkerman <very1baz1k>
2014-05-08 10:39:56 -0700

    using underscore

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function myMain(){
 
 	var list = [1,2,3,4,5,99];
-	for(var i=0,total=list.length; i<total; i++){
-		console.log(list[i]);
-	}
+	_.each(list, function(item){
+			console.log("using underscore"+item)
+	})
 }
 
 myMain();
